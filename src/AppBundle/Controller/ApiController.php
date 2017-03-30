02d52e746cb3d3d092c5c735709c4a667a7e3fa6
Mahesh Shrestha <Mahesh@192-168-1-5.tpgi.com.au>
2017-03-31 04:25:15 +1100

    added api method to update and delete article

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -13,6 +13,7 @@ class ApiController extends Controller
 {
     /**
      * @Route("/api/author/add", name="add_author")
+     * @method("POST")
      *
      */
     public function createAuthorAction(Request $request)
@@ -30,7 +31,7 @@ class ApiController extends Controller
 
     /**
      * @Route("/api/article/add", name="add_article")
-     *
+     * @method("POST")
      *
      */
     public function createArticleAction(Request $request)
@@ -59,5 +60,61 @@ class ApiController extends Controller
 
         return $response;
     }
+
+    /**
+     * @Route("/api/article/update", name="update_article")
+     * @method("POST")
+     *
+     */
+    public function updateAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $body = $request->getContent();
+        $data = json_decode($body, true);
+        $article = $em->getRepository('AppBundle:Article')->find($data['id']);
+        $repoAuthor = $em->getRepository('AppBundle:Author');
+        $author = $repoAuthor->find($data['auther_id']);
+        // Check if author posted is a valid author
+        if($author && $article){
+            $article->setAuthor($author)
+                ->setTitle($data['title'])
+                ->setContent($data['content'])
+                ->setUrl($data['url'])
+                ->setUpdatedAt(new \DateTime());
+            $em->persist($article);
+            $em->flush();
+            $response = new Response('Article updated successfully');
+        }
+        else{
+            $response = new Response('Invalid author_id and/or article_id');
+        }
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/article/Delete", name="delete_article")
+     * @method("POST")
+     *
+     */
+    public function deleteAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $body = $request->getContent();
+        $data = json_decode($body, true);
+        $article = $em->getRepository('AppBundle:Article')->find($data['id']);
+        if($article){
+            $em->remove($article);
+            $em->flush();
+            $response = new Response('Article deleted successfully');
+        }
+        else{
+            $response = new Response('Invalid article_id');
+        }
+
+        return $response;
+    }
+
+    
 
 }
