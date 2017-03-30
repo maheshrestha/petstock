80698fd72ba0a50a9b7b839419435521c16bbe60
Mahesh Shrestha <Mahesh@192-168-1-5.tpgi.com.au>
2017-03-31 05:07:52 +1100

    Bash file to backup database

---
+++ backup.sh
@@ -0,0 +1,13 @@
+# Database credentials
+ user="root"
+ password=""
+ host="localhost"
+ db_name="perstock"
+
+ backup_path="/opt/backups/database"
+ date=$(date +"%d-%b-%Y")
+
+ # Set default file permissions
+  umask 177
+ # Dump database into SQL file
+  mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
